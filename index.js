8372709d582c1df4114fb21424bf27f7bb706a6a
Peter-Roh <rmc2@naver.com>
2020-01-06 12:41:14 +0900

    learning express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hi!");
+import express from "express";
+const app = express();
+const PORT = 4000;
+
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send('Hello from home!');
+
+app.get("/", handleHome);
+
+app.listen(PORT, handleListening);

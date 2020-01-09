30ac178c7936df925e90eaef163b6a28dfd20d52
Peter-Roh <rmc2@naver.com>
2020-01-09 14:52:04 +0900

    MVC pattern, routing

--- app.js
+++ app.js
@@ -3,14 +3,11 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const app = express();
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
-
-const handleHome = (req, res) => res.send('Hello from home!');
-const handleProfile = (req, res) => res.send('You are on my profile.');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -18,8 +15,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(helmet());
 app.use(morgan("dev"));
 
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
-app.get("/", handleHome);
-app.get("/profile", handleProfile);
-
-app.listen(PORT, handleListening);
+export default app;

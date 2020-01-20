fd2b2e15b02a40df4cd86c0d23741e135600946a
Peter-Roh <rmc2@naver.com>
2020-01-20 20:46:06 +0900

    change pug, add mock db

---
+++ mockdb.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 324398,
+        title: 'Video Awesome', 
+        description: 'This is something I love', 
+        views: 24, 
+        videoFile: "https://archive.org/details/BigBuckBunny_124", 
+        creator: {
+            id: 121212, 
+            name: "Nicolas", 
+            email: "nico@las.com"
+        }
+    }, 
+    {
+        id: 123456,
+        title: 'Video Beautiful', 
+        description: 'This is something I love', 
+        views: 24, 
+        videoFile: "https://archive.org/details/BigBuckBunny_124", 
+        creator: {
+            id: 121212, 
+            name: "Nicolas", 
+            email: "nico@las.com"
+        }
+    }, 
+    {
+        id: 111111,
+        title: 'Video Nice', 
+        description: 'This is something I love', 
+        views: 24, 
+        videoFile: "https://archive.org/details/BigBuckBunny_124", 
+        creator: {
+            id: 121212, 
+            name: "Nicolas", 
+            email: "nico@las.com"
+        }
+    }, 
+    {
+        id: 5784957,
+        title: 'Video Perfect', 
+        description: 'This is something I love', 
+        views: 24, 
+        videoFile: "https://archive.org/details/BigBuckBunny_124", 
+        creator: {
+            id: 121212, 
+            name: "Nicolas", 
+            email: "nico@las.com"
+        }
+    }
+];

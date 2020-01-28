3c7d22064304e7e2a3d2f74dd7dbbddc35842f32
Peter-Roh <rmc2@naver.com>
2020-01-28 17:25:54 +0900

    upload video using db

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true, useFindAndModify: false, 
+    useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true, 
 });
 
 const db = mongoose.connection;
